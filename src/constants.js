96a597b2863377076d9f86b68c186a664e8c942a
barbeque-squared <barbeque.squared@gmail.com>
2021-05-07 22:18:57 +0200

    add some test stuff

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,36 @@
 import { Watchable } from './types/Watchable'
 
 export const CHOICES: Watchable[] = [
+  // region Season 1
   {
-    type: 'DOUBLE_EPISODE',
     season: 1,
     episode: '1-2',
-    tags: ['Twilight Sparkle', 'Mane Six'],
     title: {
       english: 'Friendship is Magic',
       german: 'Freundschaft ist Magie'
     },
-  }
+    type: 'DOUBLE_EPISODE',
+    tags: ['Twilight Sparkle', 'Mane Six'],
+  },
+  {
+    season: 1,
+    episode: 3,
+    title: {
+      english: 'The Ticket Master',
+      german: "Eine Freundin hat's nicht leicht"
+    },
+    type: 'EPISODE',
+    tags: ['Twilight Sparkle', 'Mane Six'],
+  },
+  {
+    season: 1,
+    episode: 4,
+    title: {
+      english: 'Applebuck Season',
+      german: 'Apfelschüttelernte'
+    },
+    type: 'EPISODE',
+    tags: ['Applejack'],
+  },
+  // endregion
 ]
--- src/types/Tag.js
+++ src/types/Tag.js
@@ -1,12 +1,22 @@
 // Keep in sync with Tag
 export const Tags = [
   'Twilight Sparkle',
+  'Applejack',
+  'Rarity',
   'Fluttershy',
+  'Rainbow Dash',
+  'Pinkie Pie',
   'Mane Six',
+  'Spike',
 ]
 
 // Keep in sync with Tags
 export type Tag =
   'Twilight Sparkle' |
+  'Applejack' |
+  'Rarity' |
   'Fluttershy' |
-  'Mane Six'
+  'Rainbow Dash' |
+  'Pinkie Pie' |
+  'Mane Six' |
+  'Spike'
