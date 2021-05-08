d64e4b5ccf3e0185bf35a4a0cac50cdf43a699d9
barbeque-squared <barbeque.squared@gmail.com>
2021-05-08 14:20:15 +0200

    season 4

---
+++ src/constants/season4.js
@@ -0,0 +1,242 @@
+import { Watchable } from '../types/Watchable'
+
+export const SEASON_4: Watchable[] = [
+  {
+    type: 'DOUBLE_EPISODE',
+    season: 4,
+    episode: '1-2',
+    tags: ['Twilight Sparkle', 'Mane Six', 'Discord'],
+    title: {
+      english: 'Princess Twilight Sparkle',
+      german: 'Prinzessin Twilight Sparkle'
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 3,
+    tags: ['Mane Six'],
+    title: {
+      english: 'Castle Mane-ia',
+      german: "Ponygrusel im Schloss"
+    }
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 4,
+    tags: ['Rainbow Dash'],
+    title: {
+      english: "Daring Don't",
+      german: "Wo bleibt Daring"
+    }
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 5,
+    tags: ['Cutie Mark Crusaders'],
+    title: {
+      english: "Flight to the Finish",
+      german: "Kampf bis zum Ende"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 6,
+    tags: ['Mane Six'],
+    title: {
+      english: "Power Ponies",
+      german: "Power-Ponys"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 7,
+    tags: ['Applejack', 'Fluttershy'],
+    title: {
+      english: "Bats!",
+      german: "Flutterflughund"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 8,
+    tags: ['Rarity'],
+    title: {
+      english: "Rarity Takes Manehattan",
+      german: "Rarity in Mähnhattan"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 9,
+    tags: ['Applejack', 'Pinkie Pie'],
+    title: {
+      english: "Pinky Apple Pie",
+      german: "Pinkie Apple Pie"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 10,
+    tags: ['Rainbow Dash'],
+    title: {
+      english: "Rainbow Falls",
+      german: "Rainbow kommt zur Vernunft"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 11,
+    tags: ['Twilight Sparkle', 'Discord'],
+    title: {
+      english: "Three's a Crowd",
+      german: "Drei ist Einer zu viel"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 12,
+    tags: ['Pinkie Pie'],
+    title: {
+      english: "Pinkie Pride",
+      german: "Pinkies Stolz"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 13,
+    tags: ['Applejack', 'Rarity'],
+    title: {
+      english: "Simple Ways",
+      german: "Einfache Entscheidungen"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 14,
+    tags: ['Fluttershy'],
+    title: {
+      english: "Filli Vanilli",
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 15,
+    tags: ['Twilight Sparkle', 'Cutie Mark Crusaders'],
+    title: {
+      english: "Twilight Time",
+      german: "Twilight Zeit"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 16,
+    tags: ['Fluttershy'],
+    title: {
+      english: "It Ain't Easy Being Breezies",
+      german: "Es ist nicht leicht eine Breezie zu sein"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 17,
+    tags: ['Cutie Mark Crusaders'],
+    title: {
+      english: "Somepony to Watch Over Me",
+      german: "Ein Pony das über mich wacht"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 18,
+    tags: ['Mane Six'],
+    title: {
+      english: "Maud Pie",
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 19,
+    tags: ['Rarity', 'Cutie Mark Crusaders'],
+    title: {
+      english: "For Whom the Sweetie Belle Tolls",
+      german: "Sweetie Bells bemühungen"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 20,
+    tags: ['Applejack'],
+    title: {
+      english: "Leap of Faith",
+      german: "Glaube an die Wahrheit"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 21,
+    tags: ['Rainbow Dash', 'Mane Six'],
+    title: {
+      english: "Testing, Testing, 1, 2, 3",
+      german: "Test, Test, 1, 2, 3"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 22,
+    tags: ['Mane Six'],
+    title: {
+      english: "Trade Ya",
+      german: "Die Handelsmesse"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 23,
+    tags: ['Rarity'],
+    title: {
+      english: "Inspiration Manifestation",
+      german: "Ausdruck der Inspiration"
+    },
+  },
+  {
+    type: 'EPISODE',
+    season: 4,
+    episode: 24,
+    tags: ['Spike'],
+    title: {
+      english: "Equestria Games",
+      german: "Equestria Spiele"
+    },
+  },
+  {
+    type: 'DOUBLE_EPISODE',
+    season: 4,
+    episode: '25-26',
+    tags: ['Twilight Sparkle', 'Mane Six'],
+    title: {
+      english: "Twilight's Kingdom",
+      german: "Twilights Königreich"
+    },
+  },
+]
